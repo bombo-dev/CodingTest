429190e0061445cb9c3dba627303ba30a70af4f7
Bombo <bombo96@naver.com>
2022-07-21 14:38:43 +0900

    Greedy 숫자 카드 게임, min, max 이용

---
+++ Part2/Greedy/example03-2.py
@@ -0,0 +1,12 @@
+# min, max를 이용한 답안
+
+n, m = map(int, input().split())
+
+result = 0
+
+for i in range(n):
+    data = list(map(int, input().split()))
+    min_value = min(data)
+    result = max(result, min_value)
+
+print(result)
