a47177c2d184fda8c2c6c4d3b712f7c06a4540dd
Bombo <bombo96@naver.com>
2022-07-27 13:39:03 +0900

    퀵 정렬 알고리즘 구현

---
+++ Part2/Sort/QuickSortAlgorithm.py
@@ -0,0 +1,28 @@
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def QuickSort(array, start, end):
+    if start >= end:
+        return
+    pivot = start
+    left = start + 1
+    right = end
+
+    while left <= right:
+        while left <= end and array[pivot] >= array[left]:
+            left += 1
+
+        while right > start and array[pivot] <= array[right]:
+            right -= 1
+
+        if left > right:
+            array[pivot], array[right] = array[right], array[pivot]
+
+        else:
+            array[left], array[right] = array[right], array[left]
+
+    QuickSort(array, start, right - 1)
+    QuickSort(array, right + 1, end)
+
+
+QuickSort(array, 0, len(array) - 1)
+print(array)
