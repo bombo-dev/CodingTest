d7d8f5d77cfb50a7a282432ea7c6c6c385fb6f31
Bombo <bombo96@naver.com>
2022-07-27 12:58:41 +0900

    삽입 정렬 구현

--- Part2/Sort/InsertionSortAlgorithm.py
+++ Part2/Sort/InsertionSortAlgorithm.py
@@ -1,1 +1,10 @@
 # 삽입 정렬 알고리즘 구현 예시
+
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(1, len(array)):
+    for j in range(i, 0, -1):
+        if array[j] < array[j-1]:
+            array[j-1], array[j] = array[j], array[j-1]
+
+
