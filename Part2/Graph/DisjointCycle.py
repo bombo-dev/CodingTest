7aaf3c2dd2a72bcdac1a49a3db6df8eb42b42266
Bombo <74203371+bombo-dev@users.noreply.github.com>
2022-10-30 01:35:54 +0900

    서로소 집합을 이용한 사이클 판별 알고리즘

---
+++ Part2/Graph/DisjointCycle.py
@@ -0,0 +1,38 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    else:
+        return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+# 노드의 개수와 간선(union 연산)의 개수 입력받기
+v, e = map(int, input().split())
+
+parent = [0] * (v + 1)
+for i in range(1, v + 1):
+    parent[i] = i
+
+cycle = False
+
+for _ in range(e):
+    a, b = map(int, input().split())
+    # 사이클이 발생한 경우 종료
+    if find_parent(parent, a) == find_parent(parent, b):
+        cycle = True
+        break
+    # 사이클이 발생하지 않으면
+    else:
+        union_parent(parent, a, b)
+
+if cycle:
+    print("사이클이 발생했습니다.")
+else:
+    print("사이클이 발생하지 않았습니다.")
