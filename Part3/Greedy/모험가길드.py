9b876280fdcea0a36080f68017a740dd60389d8d
Bombo <bombo96@chungwoon.ac.kr>
2022-11-05 19:48:59 +0900

    모험가 길드 주석 설명 추가

--- Part3/Greedy/모험가길드.py
+++ Part3/Greedy/모험가길드.py
@@ -3,12 +3,12 @@ fear = list(map(int, input().split()))
 
 count = 0
 result = 0
-fear = sorted(fear)
+fear = sorted(fear) # 공포도 오름 차순 정렬
 
 for i in fear:
     count += 1
-    if count >= i:
-        result += 1
+    if count >= i: # count가 현재 그룹원의 공포도보다 크거나 같은 경우
+        result += 1 # 그룹에 추가
         count = 0
 
 print(result)
