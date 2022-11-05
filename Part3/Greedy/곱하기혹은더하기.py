509af68468ab2e3724b06b76d1549b66769352b4
Bombo <bombo96@chungwoon.ac.kr>
2022-11-05 19:37:53 +0900

    Greedy 곱하기 혹은 더하기 오답 수정

--- Part3/Greedy/곱하기혹은더하기.py
+++ Part3/Greedy/곱하기혹은더하기.py
@@ -10,10 +10,11 @@ for i in range(len(S)):
     if i == 0:
         result += list[i]
     else:
-        if list[i - 1] != 0 and list[i] != 0:
-            result *= list[i]
-        else:
+        if list[i - 1] <= 1 and list[i] <= 1:
             result += list[i]
+        else:
+            result *= list[i]
 
 print(result)
 
+# 0 일 때 만이 아닌 1일 때도 상정
