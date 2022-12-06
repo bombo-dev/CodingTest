9707c5467632b0a133dad9079d6b2087c4bb17ba
Bombo <74203371+bombo-dev@users.noreply.github.com>
2022-12-06 13:56:57 +0900

    시각 [Implementation]
    String.contains(" ") -> singlequote 가 아닌 doublequote를 사용해야 한다.

---
+++ Part2/Implementation/java/Question2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class Question2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String N = br.readLine().trim();
+
+        int result = 0;
+
+        for(int i = 0; i < Integer.parseInt(N) + 1; i++){
+            for(int j = 0; j <= 59; j++){
+                for (int k = 0; k <= 59; k++){
+                    String s = i + " " + j + " " + k;
+                    if(s.contains("3")){
+                        result += 1;
+                    }
+                }
+            }
+        }
+        System.out.print(result);
+    }
+}
