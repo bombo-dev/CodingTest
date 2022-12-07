c6cd49dddf30986b02a9ce50237771866e5ce9bf
Bombo <74203371+bombo-dev@users.noreply.github.com>
2022-12-07 17:37:22 +0900

    BFS 정의 구현 [BFS]

---
+++ Part2/DFS,BFS/java/BFS.java
@@ -0,0 +1,60 @@
+import java.util.*;
+public class bfs {
+    public static boolean[] visited = new boolean[9];
+    public static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
+    public static void bfsImpl(int start){
+        ArrayDeque<Integer> q = new ArrayDeque<>(); // new LinkedList<>();
+        q.offer(start);
+        visited[start] = true;
+
+        while(!q.isEmpty()){
+            int x = q.pollFirst();
+          //int x = q.poll();
+            System.out.print(x + " ");
+            for(int i = 0; i < graph.get(x).size(); i++){
+                int y = graph.get(x).get(i);
+                if(!visited[y]){
+                    q.offer(y);
+                    visited[y] = true;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args){
+
+        for(int i = 0; i < 9; i++){
+            ArrayList<Integer> list = new ArrayList<>();
+            graph.add(list);
+        }
+
+        graph.get(1).add(2);
+        graph.get(1).add(3);
+        graph.get(1).add(8);
+
+        graph.get(2).add(1);
+        graph.get(2).add(7);
+
+        graph.get(3).add(1);
+        graph.get(3).add(4);
+        graph.get(3).add(5);
+
+        graph.get(4).add(3);
+        graph.get(4).add(5);
+
+        graph.get(5).add(3);
+        graph.get(5).add(4);
+
+        graph.get(6).add(7);
+
+        graph.get(7).add(2);
+        graph.get(7).add(6);
+        graph.get(7).add(8);
+
+        graph.get(8).add(1);
+        graph.get(8).add(7);
+
+        bfsImpl(1);
+    }
+}
