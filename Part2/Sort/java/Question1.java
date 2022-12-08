8a16863acb4d0d090b748bd5b4696c1b1f3ffac0
Bombo <74203371+bombo-dev@users.noreply.github.com>
2022-12-08 17:29:29 +0900

    위에서 아래로 [Sort]

---
+++ Part2/Sort/java/Question1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class Question1 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine().trim());
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < count; i++){
+            int value = Integer.parseInt(br.readLine().trim());
+            list.add(value);
+        }
+        list.sort(Collections.reverseOrder());
+        for(int value : list){
+            System.out.print(value + " ");
+        }
+    }
+}
