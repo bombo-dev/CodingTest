de8f7c4b178a2bba8e117bc9c55f81f774cbd0b9
Bombo <74203371+bombo-dev@users.noreply.github.com>
2022-12-08 17:28:35 +0900

    객체를 이용한 정렬 오버라이드 [Sort]

---
+++ Part2/Sort/java/ObjectSort.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Fruit implements Comparable<Fruit> {
+    private String name;
+    private int price;
+
+    Fruit(String name, int price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getPrice(){
+        return this.price;
+    }
+
+    @Override
+    public int compareTo(Fruit fruit){
+        return this.price - fruit.price;
+    }
+}
+public class ObjectSort {
+    public static void main(String[] args){
+        List<Fruit> list = new ArrayList<>();
+
+        list.add(new Fruit("바나나", 2));
+        list.add(new Fruit("사과", 1));
+        list.add(new Fruit("파인애플", 3));
+
+        Collections.sort(list);
+        for(Fruit val : list){
+            System.out.print("(" + val.getName() + "," + val.getPrice() + ") ");
+        }
+    }
+}
