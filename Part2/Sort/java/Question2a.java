9a578db6c08d490db4a757f4d090b9bf4373fb32
Bombo <74203371+bombo-dev@users.noreply.github.com>
2022-12-08 17:33:23 +0900

    성적이 낮은 순서로 학생 출력하기 Map 이용 [Sort]

---
+++ Part2/Sort/java/Question2a.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+import java.util.Map.Entry;
+
+public class Question2a {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine().trim());
+
+        Map<String, Integer> student = new HashMap<>();
+        for(int i = 0; i < count; i++){
+            String[] value = br.readLine().split(" ");
+            student.put(value[0], Integer.parseInt(value[1]));
+        }
+
+        List<Entry<String, Integer>> entry = new ArrayList<>(student.entrySet());
+        Collections.sort(entry, new Comparator<Entry<String, Integer>>(){
+            @Override
+            public int compare(Entry<String, Integer> obj1, Entry<String,Integer> obj2){
+                return obj1.getValue().compareTo(obj2.getValue());
+            }
+        });
+
+        for(Entry<String, Integer> value : entry){
+            System.out.print(value.getKey() +" ");
+        }
+    }
+}
