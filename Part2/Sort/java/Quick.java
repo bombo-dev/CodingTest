c0b26b9f1d7c9942381d480960af7555d97dce42
Bombo <74203371+bombo-dev@users.noreply.github.com>
2022-12-08 17:27:03 +0900

    퀵 정렬 정의 코드 [Sort]

---
+++ Part2/Sort/java/Quick.java
@@ -0,0 +1,37 @@
+public class Quick {
+
+    public static void quickSort(int[] arr, int start, int end){
+        if(start >= end)
+            return;
+        int pivot = start;
+        int left = start + 1;
+        int right = end;
+        while(left <= right){
+            while (left <= end && arr[left] <= arr[pivot]) left++;
+            while (right > start && arr[right] >= arr[pivot]) right--;
+
+            // 겹치는 순간 피벗이 제 위치를 찾는다.
+            if (left > right) {
+                int temp = arr[pivot];
+                arr[pivot] = arr[right];
+                arr[right] = temp;
+            } else {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+            }
+        }
+        // 오른쪽에서 시작했을 경우 재 분할을 했을 때 start 가 0 이 아님
+        quickSort(arr, start, right - 1);
+        quickSort(arr, right + 1, end);
+    }
+
+    public static void main(String[] args){
+        int[] arr = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8};
+        quickSort(arr, 0, arr.length - 1);
+
+        for(int value: arr){
+            System.out.print(value + " ");
+        }
+    }
+}
