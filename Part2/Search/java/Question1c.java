059e54ba9e94becb65410ef0acb030c2accd81eb
Bombo <74203371+bombo-dev@users.noreply.github.com>
2022-12-09 13:51:22 +0900

    부품 찾기 Set 자료구조 이용 [Search]

--- Part2/Search/java/Question1c.java
+++ Part2/Search/java/Question1c.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
 
+// Set 자료구조 이용
 public class Question1c {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
