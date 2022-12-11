d5a6672e5373aa7139863a0783658031de685a23
Bombo <74203371+bombo-dev@users.noreply.github.com>
2022-12-11 20:05:50 +0900

    바닥공사 [Dynamic Programming]

---
+++ Part2/DynamicProgramming/java/Question3.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Question3 {
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        long[] dp = new long[1001];
+        dp[1] = 1;
+        dp[2] = 3;
+        for(int i = 3; i <= n; i++){
+            dp[i] = (dp[i - 2] * 2) + dp[i - 1];
+        }
+        System.out.print(dp[n] % 796796);
+    }
+}
