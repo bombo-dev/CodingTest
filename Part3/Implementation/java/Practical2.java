ed523fe7edcf10292a4bd8ad12060ab8030d203b
Bombo <74203371+bombo-dev@users.noreply.github.com>
2022-12-20 11:31:15 +0900

    럭키 스트레이트 [Implementation]

---
+++ Part3/Implementation/java/Practical2.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Practical2 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String value = br.readLine();
+        int half = value.length() / 2;
+
+        int sum1 = 0;
+        int sum2 = 0;
+
+        for(int i = 0; i < half; i++){
+            sum1 += Character.getNumericValue(value.charAt(i));
+        }
+        for(int i = half; i < value.length(); i++){
+            sum2 += Character.getNumericValue(value.charAt(i));
+        }
+
+        if(sum1 == sum2){
+            System.out.print("LUCKY");
+        } else{
+            System.out.print("READY");
+        }
+    }
+}
