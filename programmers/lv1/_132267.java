be7fd4e96e7978159d07b977a6ca85884207d720
Bombo <74203371+bombo-dev@users.noreply.github.com>
2022-12-27 11:10:16 +0900

    콜라 문제 [Implementation]

---
+++ programmers/lv1/_132267.java
@@ -0,0 +1,23 @@
+public class _132267 {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        int temp = 0;
+        while(n > 0){
+            temp = 0;
+            if(n % a == 0){
+                answer += (n/a) * b;
+                n = (n/a) * b;
+            } else {
+                answer += (n/a) * b;
+                temp = (n % a);
+                n = (n/a) * b;
+            }
+            if(n + temp >= a){
+                n += temp;
+            } else {
+                break;
+            }
+        }
+        return answer;
+    }
+}
