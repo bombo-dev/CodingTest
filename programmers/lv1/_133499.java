e8fc8e3e75fde91ca55161c658bdb02179a96ec6
Bombo <74203371+bombo-dev@users.noreply.github.com>
2022-12-27 11:11:37 +0900

    옹알이(2) [Implemenation]

---
+++ programmers/lv1/_133499.java
@@ -0,0 +1,27 @@
+public class _133499 {
+    public static int solution(String[] babbling) {
+        int answer = 0;
+        boolean check = false;
+        String[] al = {"aya", "ye", "woo", "ma"};
+        for (int i = 0; i < babbling.length; i++) {
+            check = false;
+            for (int j = 0; j < al.length; j++) {
+                babbling[i].replaceAll(al[j], Character.toString(j));
+                if (babbling[i].contains(Character.toString(j) + Character.toString(j))) {
+                    check = true;
+                    break;
+                }
+            }
+            if (check == true) {
+                break;
+            } else {
+                if (!babbling[i].matches("/[a-z]/g"))
+                    break;
+                else {
+                    answer += 1;
+                }
+            }
+        }
+        return answer;
+    }
+}
