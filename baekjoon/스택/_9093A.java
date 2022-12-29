70ab43e32dc2c2610a365f0ebaa73cf6db8c2e90
Bombo <74203371+bombo-dev@users.noreply.github.com>
2022-12-29 17:27:10 +0900

    문자열 뒤집기_개선1 [Brute Force]

---
+++ baekjoon/스택/_9093A.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+public class _9093A {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Character> stack = new Stack<>();
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            String[] value = br.readLine().split(" ");
+            for (String val : value) {
+                for (int j = 0; j < val.length(); j++) {
+                    stack.push(val.charAt(j));
+                }
+                while (!stack.isEmpty()) {
+                    sb.append(stack.pop());
+                }
+                sb.append(" ");
+            }
+            System.out.println(sb);
+        }
+    }
+}
