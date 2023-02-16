ca4418b5e064faddb88fcdfe683ead95d7f9dc9d
Bombo <bombo96@chungwoon.ac.kr>
2023-02-17 00:03:37 +0900

    - DynamicProgramming : 음수 피보나치

---
+++ Java/codingtest/src/codeTree/NegativeFibo_A.java
@@ -0,0 +1,23 @@
+package codeTree;
+
+import java.util.Scanner;
+
+public class NegativeFibo_A {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] dp = new int[(int)1e9];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= N; i++){
+            dp[i] = dp[i - 2] - dp[i - 1];
+        }
+
+
+        for(int i = 0; i <= N; i++){
+            System.out.println(i + " : " + dp[i]);
+        }
+    }
+}
