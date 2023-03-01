2d30f774bec98c628e6b949c631c3dc5fffc21c6
Bombo <bombo96@chungwoon.ac.kr>
2023-03-01 12:21:32 +0900

    - programmers : H-index (정렬) 다른 풀이 (https://school.programmers.co.kr/learn/courses/30/lessons/42747)

---
+++ Java/codingtest/src/programmers/lv2/_42747.java
@@ -0,0 +1,19 @@
+package programmers.lv2;
+import java.util.*;
+public class _42747 {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+
+        for(int i = 0; i < citations.length; i++){
+            int h = citations.length - i;
+
+            if(citations[i] >= h){
+                answer = h;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
