5955dbd283b06ecb8314a623681f555714fcc1c2
Bombo <bombo96@chungwoon.ac.kr>
2023-04-16 15:38:11 +0900

    - baekjoon : 세 수의 합 [이진 탐색, 중간에서 만나기] (https://www.acmicpc.net/problem/2995)

---
+++ Java/codingtest/src/baekjoon/_2995.java
@@ -0,0 +1,45 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+// 더블 포인터, 이분 탐색, 시간 복잡도를 철저하게 계산해야 했던 문제
+// 중간에서 만나기 라는 기술을 사용 가능 (Meet in the middle)
+public class _2995 {
+    static Long[] arr;
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new Long[N];
+        for(int i = 0; i < N; i++) {
+            arr[i] = Long.parseLong(br.readLine());
+        }
+        // NlogN
+        Arrays.sort(arr);
+
+        ArrayList<Long> newSum = new ArrayList<>();
+        //N^2
+        for(int i = 0; i < N; i++){
+            for(int j = i; j < N; j++) {
+                newSum.add(arr[i] + arr[j]);
+            }
+        }
+        //NlogN
+        Collections.sort(newSum);
+
+        // N^2 + NlogN + NlogN
+
+        for(int i = N - 1; i >= 0; i--) {
+            for(int j = i; j >= 0; j--) {
+                long nextValue = arr[i] - arr[j];
+
+                // N^2logN
+                if(Collections.binarySearch(newSum, nextValue) >= 0) {
+                    System.out.print(arr[i]);
+                    return;
+                }
+            }
+        }
+    }
+}
