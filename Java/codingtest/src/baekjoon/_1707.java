f02f6583a794bf67e4a653956f6d6198c740e7a1
Bombo <bombo96@chungwoon.ac.kr>
2023-04-20 17:42:23 +0900

    - baekjoon : 이분 그래프 [DFS, BFS] + BFS 풀이(https://www.acmicpc.net/problem/1707)

--- Java/codingtest/src/baekjoon/_1707.java
+++ Java/codingtest/src/baekjoon/_1707.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 public class _1707 {
 
-    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    static ArrayList<ArrayList<Integer>> graph;
     static int[] color;
     static final int RED = -1;
     static final int BLACK = 1;
@@ -17,6 +17,7 @@ public class _1707 {
 
         while(T-->0) {
             result = true;
+            graph = new ArrayList<>();
             StringTokenizer st = new StringTokenizer(br.readLine(),  " ");
             int V = Integer.parseInt(st.nextToken());
             int E = Integer.parseInt(st.nextToken());
@@ -39,11 +40,9 @@ public class _1707 {
             }
 
             for(int i = 1; i < V + 1; i++) {
-                if(!result)
-                    break;
-
                 if(color[i] == 0) {
-                    dfs(i, RED);
+//                    dfs(i, RED);
+                    bfs(i, RED);
                 }
             }
 
@@ -64,11 +63,31 @@ public class _1707 {
         for(int end : graph.get(start)) {
             if(color[end] == colorValue) {
                 result = false;
-                return;
             }
 
             if(color[end] == 0) {
                 dfs(end, colorValue * -1);
+            }
+        }
+    }
+
+    static void bfs(int start, int colorValue) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        color[start] = colorValue;
+
+        while(!q.isEmpty()) {
+            int pre = q.poll();
+
+            for(int end : graph.get(pre)) {
+                if(color[end] == color[pre]) {
+                    result = false;
+                }
+
+                if(color[end] == 0) {
+                    q.offer(end);
+                    color[end] = color[pre] * -1;
+                }
             }
         }
     }
